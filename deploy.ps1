2a5797beb295d6cc9a7cf80b598de8c3c0a58587
Caputi <lcaputi2@gmail.com>
2025-12-02 15:21:56 -0300

    chore: fix automatized deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,10 +1,17 @@
 # Pega a mensagem do último commit
 $LastCommitMessage = git log -1 --pretty=%s
-Write-Host " Iniciando deploy com a mensagem: '$LastCommitMessage'" -ForegroundColor Cyan
 
-# Configura as variáveis
+# Verifica se pegou a mensagem, senão usa uma padrão
+if (-not $LastCommitMessage) {
+    $LastCommitMessage = "Atualização do site"
+}
+
+Write-Host "Iniciando Deploy Automatizado" -ForegroundColor Cyan
+Write-Host "Mensagem do Commit: '$LastCommitMessage'" -ForegroundColor Yellow
+
+# Configura as variáveis de ambiente para a sessão atual
 $env:GIT_USER = "CaputiDev"
 $env:DEPLOYMENT_MESSAGE = $LastCommitMessage
 
-# Roda o comando
+# Executa o comando de deploy
 npm run deploy
